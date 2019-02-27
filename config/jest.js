f9bc416cd73720fd2bd7c9030e8a620109e1ee3e
7korobi <7korobi@gmail.com>
2019-02-27 23:03:21 +0900

    windows build.

---
+++ config/jest.js
@@ -0,0 +1,23 @@
+const { is_windows } = require("./os")
+
+let testRegex = "//__tests__//.*spec.coffee$"
+if ( is_windows ) { testRegex = "\\\\__tests__\\\\.*spec.coffee$" }
+
+
+module.exports = {
+  testRegex,
+  moduleFileExtensions: [
+    "coffee",
+    "js",
+  ],
+  moduleDirectories: [
+    "node_modules",
+    "."
+  ],
+  transform: {
+    "^.+\\.coffee$": "<rootDir>/config/coffee-jest.js",
+    "^.+\\.js$": "babel-jest",
+    "^.+\\.pug$": "pug-jest",
+    "^.+\\.vue$": "vue-jest"
+  }
+}
