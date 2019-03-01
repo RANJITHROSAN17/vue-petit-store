81dd03e46c457925098351ec2a413b1da4ea7d83
7korobi <7korobi@BA-1810001-MAC.local>
2019-03-01 16:59:31 +0900

    jest fix.

--- config/jest.js
+++ config/jest.js
@@ -1,23 +1,18 @@
-const { is_windows } = require("./os")
+const { is_windows } = require("./os");
 
-let testRegex = "//__tests__//.*spec.coffee$"
-if ( is_windows ) { testRegex = "\\\\__tests__\\\\.*spec.coffee$" }
-
+let testRegex = "/__tests__/.*spec.coffee$";
+if (is_windows) {
+  testRegex = "\\\\__tests__\\\\.*spec.coffee$";
+}
 
 module.exports = {
   testRegex,
-  moduleFileExtensions: [
-    "coffee",
-    "js",
-  ],
-  moduleDirectories: [
-    "node_modules",
-    "."
-  ],
+  moduleFileExtensions: ["coffee", "js"],
+  moduleDirectories: ["node_modules", "."],
   transform: {
     "^.+\\.coffee$": "<rootDir>/config/coffee-jest.js",
     "^.+\\.js$": "babel-jest",
     "^.+\\.pug$": "pug-jest",
     "^.+\\.vue$": "vue-jest"
   }
-}
+};
